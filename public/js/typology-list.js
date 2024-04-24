cca6bb990b81b934aed5e423c58ac077b266db72
CarballoDa <161361087+CarballoDa@users.noreply.github.com>
2024-04-24 20:45:12 +0200

    Add files via upload

--- public/js/typology-list.js
+++ public/js/typology-list.js
@@ -5,70 +5,83 @@ const list = {
     title : "",
     typologies : [],
     items : [],
-    getSortAsc : function(typologies){
+    getTypoSortAsc : function(typologies){
         return typologies.sort();
     },
-    getSortDesc : function(typologies){
+    getTypoSortDesc : function(typologies){
         return typologies.sort(function(a,b){
             if(a<b){return 1;}
             if(a>b){return -1;}
             return 0;
         });
     },
+    getItmSortAsc : function(items){
+        return typologies.sort();
+    },
+    getItmSortDesc : function(items){
+        return items.sort(function(a,b){
+            if(a.title<b.title){return 1;}
+            if(a.title>b.title){return -1;}
+            return 0;
+        });
+    },
 };
-
 /* 
 * List Title Managment
 * Function createListTitle()
 */
-const newListTileButton = document.getElementById("newListTitle");
 function createListTitle(){ 
     list.title = (document.getElementById("inputListTitle").value.length > 0) ? document.getElementById("inputListTitle").value : 'Empty Title List';
     document.getElementById("inputListTitle").value = list.title;
-    document.getElementById('newListTitle').classList.remove('visible');
-    document.getElementById('newListTitle').classList.add('invisible');
-    document.getElementById('removeListTitle').classList.remove('invisible');
-    document.getElementById('removeListTitle').classList.add('visible');
     document.getElementById("inputListTitle").disabled = true;
+    changeClassById('newListTitle', 'visible', 'invisible');
+    changeClassById('removeListTitle', 'invisible', 'visible');    
     console.log(list);
 }
+const newListTileButton = document.getElementById("newListTitle");
 newListTileButton.addEventListener('click', createListTitle, false);
 /* 
 * Function removeListTitle()
+* Info : Will be deleted all list content (title, typos, items)
 */
-const removeListTitleButton = document.getElementById("removeListTitle");
 function removeListTitle(){ 
     list.title = document.getElementById("inputListTitle").value = "";
-    document.getElementById('newListTitle').classList.remove('invisible');
-    document.getElementById('newListTitle').classList.add('visible');
-    document.getElementById('removeListTitle').classList.remove('visible');
-    document.getElementById('removeListTitle').classList.add('invisible');
     document.getElementById("inputListTitle").disabled = false;
+    changeClassById('removeListTitle', 'visible', 'invisible');
+    changeClassById('newListTitle', 'invisible', 'visible');    
     console.log(list);
 }
+const removeListTitleButton = document.getElementById("removeListTitle");
 removeListTitleButton.addEventListener('click', removeListTitle, false);
 /* 
 * List Typologies Managment
+* Function addTypology()
 */
-const newTypologyButton = document.getElementById("newTypology");
 function addTypology(){ 
     list.typologies.push(document.getElementById("inputTipologyTitle").value);
     console.log(list);
 }
+const newTypologyButton = document.getElementById("newTypology");
 newTypologyButton.addEventListener('click', addTypology, false);
-
+/* 
+* Function removeTypology()
+* Info : Typologies in use on the list can't be removed
+*/
+function removeTypology(id){}
 /* 
 * List Items Managment
+* Function addItem()
 */
-const newItemButton = document.getElementById("newItem");
 function addItem(){ 
     list.items.push({title : document.getElementById("inputItemTitle").value, quantity : document.getElementById("inputItemQuantity").value});
     console.log(list);
 }
+const newItemButton = document.getElementById("newItem");
 newItemButton.addEventListener('click', addItem, false);
-
-
-
+/* 
+* Function removeItem()
+*/
+function removeItem(id){}
 /*
 * Preventing key enter usage
 */
@@ -80,3 +93,11 @@ window.addEventListener('keydown',function(e) {
         }
     }
 }, true);
+/*
+* Function changeClassById(id. actualClass, newClass)
+* Changes an id css class removing actualClass and adding newClass
+*/
+function changeClassById(id, actualClass, newClass){
+    document.getElementById(id).classList.remove(actualClass);
+    document.getElementById(id).classList.add(newClass);
+}
