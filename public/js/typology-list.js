a96c6827dd225d67924f63d62216f5aa9c492c85
CarballoDa <161361087+CarballoDa@users.noreply.github.com>
2024-04-24 18:44:35 +0200

    Add files via upload

---
+++ public/js/typology-list.js
@@ -0,0 +1,6 @@
+const newListTile = document.getElementById("newListTitle");
+function createListTitle(){ 
+    document.getElementById("inputListTitle").value; 
+
+}
+newListTitle.addEventListener('click', createListTitle, false);
