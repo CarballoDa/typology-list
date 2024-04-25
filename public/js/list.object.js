64f8f629d2930a230d86f979da4a7f4ccce81195
CarballoDa <161361087+CarballoDa@users.noreply.github.com>
2024-04-25 18:23:30 +0200

    Add files via upload

--- public/js/list.object.js
+++ public/js/list.object.js
@@ -1,33 +1,63 @@
 /*
 * Object List Declaration
 */
-export const list = {
+export const List = {
     title : "",
     typologies : [],
     items : [],
     addTitle : function(value){
-        this.title = value;
+        return (value.length === 0) ? alert('Title can not be empty') : this.title = value;
     },
     addTypo : function(value){
         this.typologies.push(value);
     },
+    deleteTypo : function(id){
+        switch(id){
+            case 0:
+            this.typologies.shift();
+            break;
+            case (id === this.typologies.length - 1):
+            this.typologies.pop();
+            break;
+            default:
+            this.typologies.splice(id - 1, 1);
+        }
+    },
     addItem : function(valueA, valueB){
         this.items.push({title : valueA, quantity : valueB});
     },
-    getTypoSortAsc : function(){
+    deleteItem : function(id){
+        switch(id){
+            case 0:
+            this.items.shift();
+            break;
+            case (id === this.items.length - 1):
+            this.items.pop();
+            break;
+            default:
+            this.items.splice(id - 1, 1);
+        }
+    },
+    getTypos : function(){
+        return this.typologies;
+    },
+    getTyposSortAsc : function(){
         return this.typologies.sort();
     },
-    getTypoSortDesc : function(){
+    getTyposSortDesc : function(){
         return this.typologies.sort(function(a,b){
             if(a<b){return 1;}
             if(a>b){return -1;}
             return 0;
         });
     },
-    getItmSortAsc : function(){
+    getItems : function(){
+        return this.items;
+    },
+    getItemsSortAsc : function(){
         return this.items.sort();
     },
-    getItmSortDesc : function(){
+    getItemsSortDesc : function(){
         return this.items.sort(function(a,b){
             if(a.title<b.title){return 1;}
             if(a.title>b.title){return -1;}
--- public/js/typology-list.js
+++ public/js/typology-list.js
@@ -1,14 +1,15 @@
-import { list } from './list.object.js';
+import { List } from './list.object.js';
 /* 
 * List Title Managment
 * Function createListTitle()
 */
 function createListTitle(){ 
-    list.addTitle((document.getElementById("inputListTitle").value.length > 0) ? document.getElementById("inputListTitle").value : 'Empty Title List');
-    document.getElementById("inputListTitle").value = list.title;
+    List.addTitle((document.getElementById("inputListTitle").value.length > 0) ? document.getElementById("inputListTitle").value : 'Empty Title List');
+    document.getElementById("inputListTitle").value = List.title;
     document.getElementById("inputListTitle").disabled = true;
-    changenewListTitleButton('newListTitle', 'removeListTitle');
-    console.log(list);
+    changeVisivilityStateById('newListTitle', 'visible', 'invisible');
+    changeVisivilityStateById('removeListTitle', 'invisible', 'visible');
+    changeVisivilityStateById('listActions', 'invisible', 'visible');
 }
 const newListTileButton = document.getElementById("newListTitle");
 newListTileButton.addEventListener('click', createListTitle, false);
@@ -17,10 +18,11 @@ newListTileButton.addEventListener('click', createListTitle, false);
 * Info : Will be deleted all list content (title, typos, items)
 */
 function removeListTitle(){ 
-    list.addTitle(document.getElementById("inputListTitle").value = "");
+    List.addTitle(document.getElementById("inputListTitle").value = "");
     document.getElementById("inputListTitle").disabled = false;
-    changenewListTitleButton('removeListTitle', 'newListTitle');
-    console.log(list);
+    changeVisivilityStateById('newListTitle', 'invisible', 'visible');
+    changeVisivilityStateById('removeListTitle', 'visible', 'invisible');   
+    changeVisivilityStateById('listActions', 'visible', 'invisible');
 }
 const removeListTitleButton = document.getElementById("removeListTitle");
 removeListTitleButton.addEventListener('click', removeListTitle, false);
@@ -29,8 +31,10 @@ removeListTitleButton.addEventListener('click', removeListTitle, false);
 * Function addTypology()
 */
 function addTypology(){ 
-    list.addTypo(document.getElementById("inputTipologyTitle").value);
-    console.log(list);
+    List.addTypo(document.getElementById("inputTypologyTitle").value);    
+    loadTypologiesHtml();
+    document.getElementById("inputTypologyTitle").value = "";
+    document.getElementById("inputTypologyTitle").focus();
 }
 const newTypologyButton = document.getElementById("newTypology");
 newTypologyButton.addEventListener('click', addTypology, false);
@@ -38,21 +42,51 @@ newTypologyButton.addEventListener('click', addTypology, false);
 * Function removeTypology()
 * Info : Typologies in use on the list can't be removed
 */
-function removeTypology(id){}
+export function removeTypology(id){
+    List.deleteTypo(id);
+    loadTypologiesHtml();  
+}
+/* 
+* Function loadTypologiesHtml()
+* Info : Prepare the HTML with typos info and actions and load the HTML into an specific id
+*/
+function loadTypologiesHtml(){
+    document.getElementById("typologiesList").innerHTML = '';
+    List.getTyposSortAsc().forEach((element, index) => {
+        document.getElementById("typologiesList").innerHTML += `<li class="list-group-item">${element} <button onclick="removeTypology(${index})" type="button" class="btn btn-outline-warning float-sm-end pt-1 pb-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path></svg></button></li>`;
+    });
+}
 /* 
 * List Items Managment
 * Function addItem()
 */
 function addItem(){ 
-    list.addItem(document.getElementById("inputItemTitle").value, document.getElementById("inputItemQuantity").value);
-    console.log(list);
+    List.addItem(document.getElementById("inputItemTitle").value, document.getElementById("inputItemQuantity").value);
+    loadItemsHtml();
+    document.getElementById("inputItemTitle").value = "";
+    document.getElementById("inputItemQuantity").value = 1;
+    document.getElementById("inputItemTitle").focus();
 }
 const newItemButton = document.getElementById("newItem");
 newItemButton.addEventListener('click', addItem, false);
 /* 
 * Function removeItem()
 */
-function removeItem(id){}
+function removeItem(id){
+    List.deleteItem(id);
+    loadItemsHtml();
+}
+/* 
+* Function loadItemsHtml()
+* Info : Prepare the HTML with items info and actions and load the HTML into an specific id
+*/
+function loadItemsHtml(){
+    document.getElementById("itemsList").innerHTML = '';
+    List.getItems().forEach((element, index) => {
+        document.getElementById("itemsList").innerHTML += `<li class="list-group-item">${element.title}    X    ${element.quantity} <button type="button" class="btn btn-outline-warning float-sm-end pt-1 pb-1"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"></path><path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"></path></svg></button></li>`;
+    });
+}
+/********************************************************************************************************/
 /*
 * Preventing key enter usage
 */
@@ -65,10 +99,10 @@ window.addEventListener('keydown',function(e) {
     }
 }, true);
 /*
-* Function changeClassById(id. actualClass, newClass)
-* Changes an id css class removing actualClass and adding newClass
+* Function changeVisivilityStateById(id. oldClass, newClass)
+* Changes an visible or invisible id css class removing oldClass and adding newClass
 */
-function changenewListTitleButton(idA, idB){
-    document.getElementById(idA).classList.add('btnHidden');
-    document.getElementById(idB).classList.remove('btnHidden');
+function changeVisivilityStateById(id, oldClass, newClass){
+    document.getElementById(id).classList.remove(oldClass);
+    document.getElementById(id).classList.add(newClass);    
 }
