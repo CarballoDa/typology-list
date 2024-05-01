4a395890af51c824446ed841e074968202b499f0
David Carballo Izquierdo <161361087+CarballoDa@users.noreply.github.com>
2024-05-01 11:38:57 +0200

    Added via upoad
    The objective for this branch is to change the code :
    - From JavaScript to Typescript :
    1) list.class.ts Done!. Compiled to list.class.js
    2) typology-list.ts work in progress.  Complied to typology-list.js
    - From Bootstrap 5 to Tailwind CSS
    1) index.html pendding

--- public/js/list.class.js
+++ public/js/list.class.js
@@ -1,18 +1,103 @@
-/*
-* Class List Declaration
-*/
-export class List{
-
-    addTitle(value){
-        this.title = value;
+"use strict";
+exports.__esModule = true;
+exports.List = void 0;
+var List = /** @class */ (function () {
+    function List() {
+        this.title = '';
     }
-
-    addTypo(value){
+    List.prototype.addTitle = function (title) {
+        this.title = (title.length === 0) ? title : 'My List';
+    };
+    List.prototype.removeTitle = function () {
+        this.title = '';
+    };
+    List.prototype.addTypo = function (value) {
+        if (value.length === 0) {
+            return alert('Typology title can not be empty');
+        }
         this.typologies.push(value);
-    }
-
-    addItem(valueA, valueB){
-        this.items.push({title : valueA, quantity : valueB});
-    }
- 
-}
+    };
+    List.prototype.deleteTypo = function (id) {
+        switch (true) {
+            case (id === 0):
+                this.typologies.shift();
+                break;
+            case (id === this.typologies.length - 1):
+                this.typologies.pop();
+                break;
+            default:
+                this.typologies.splice(id, 1);
+        }
+    };
+    List.prototype.addItem = function (valueA, valueB, valueC) {
+        if (valueA.length === 0) {
+            return alert('Item title can not be empty');
+        }
+        this.items.push({ title: valueA, quantity: valueB, typology: valueC });
+    };
+    List.prototype.deleteItem = function (id) {
+        switch (true) {
+            case (id === 0):
+                this.items.shift();
+                break;
+            case (id === this.items.length - 1):
+                this.items.pop();
+                break;
+            default:
+                this.items.splice(id, 1);
+        }
+    };
+    List.prototype.getTypos = function () {
+        return this.typologies;
+    };
+    List.prototype.getTyposSortAsc = function () {
+        return this.typologies.sort();
+    };
+    List.prototype.getTyposSortDesc = function () {
+        return this.typologies.sort(function (a, b) {
+            if (a < b) {
+                return 1;
+            }
+            if (a > b) {
+                return -1;
+            }
+            return 0;
+        });
+    };
+    List.prototype.getItems = function () {
+        return this.items;
+    };
+    List.prototype.getItemsSortAsc = function () {
+        return this.items.sort();
+    };
+    List.prototype.getItemsSortDesc = function () {
+        return this.items.sort(function (a, b) {
+            if (a.title < b.title) {
+                return 1;
+            }
+            if (a.title > b.title) {
+                return -1;
+            }
+            return 0;
+        });
+    };
+    List.prototype.loadImportTypologies = function (values) {
+        var _this = this;
+        values.forEach(function (element) {
+            if (element.length > 0) {
+                _this.addTypo(element);
+            }
+        });
+    };
+    List.prototype.loadImportItems = function (values) {
+        var _this = this;
+        values.forEach(function (element) {
+            if (element.length > 0) {
+                var values_1 = element.split(';');
+                _this.addItem(values_1[1], values_1[2], values_1[0]);
+            }
+        });
+    };
+    return List;
+}());
+exports.List = List;
--- public/js/typology-list.js
+++ public/js/typology-list.js
@@ -1,243 +1,27 @@
-import { List } from './list.object.js';
-/* 
+"use strict";
+exports.__esModule = true;
+var list_class_1 = require("./list.class");
+list = new list_class_1.List();
+/*
+* Listeners
+*/
+var newListTileButton = document.getElementById("inputListTitleButton");
+newListTileButton === null || newListTileButton === void 0 ? void 0 : newListTileButton.addEventListener('click', createListTitle, false);
+/*
+* Functions
+*/
+/*
 * List Title Managment
 * Function createListTitle()
 * Getting and checking the title input value, list creation by title
 */
-function createListTitle(){ 
-    List.addTitle((document.getElementById("inputListTitle").value.length > 0 && List.title === null) ? document.getElementById("inputListTitle").value : List.title);
-    document.getElementById("inputListTitle").value = List.title;
+function createListTitle() {
+    list.addTitle((document.getElementById("inputListTitle").value.length > 0 && list.title === null) ? document.getElementById("inputListTitle").value : list.title);
+    document.getElementById("inputListTitle").value = list.title;
     ableOrDisableListTools(true, false);
     changeVisivilityStateById('listActions', 'invisible', 'visible');
 }
-const newListTileButton = document.getElementById("inputListTitleButton");
-newListTileButton.addEventListener('click', createListTitle, false);
-/* 
-* Function removeListTitle()
-* Deleting all list content (title, typos, items)
-*/
-function removeListTitle(){
-    if(confirm('Do you want to delete the list?')){
-        document.getElementById("inputListTitle").value = ""; 
-        List.deleteTitle();
-        ableOrDisableListTools(false, true);
-        changeVisivilityStateById('listActions', 'visible', 'invisible');
-        document.getElementById("typologiesList").innerHTML = '';
-        document.getElementById("selectItemTypo").innerHTML = '';
-        document.getElementById("itemsList").innerHTML = '';        
-    }
-}
-const removeListTitleButton = document.getElementById("removeListTitle");
-removeListTitleButton.addEventListener('click', removeListTitle, false);
-/* 
-* List Typologies Managment
-* Function addTypology()
-* Getting the typology input value, typology creation
-*/
-function addTypology(){ 
-    List.addTypo(document.getElementById("inputTypologyTitle").value);    
-    loadTypologiesHtml();
-    document.getElementById("inputTypologyTitle").value = "";
-    document.getElementById("inputTypologyTitle").focus();
-}
-const newTypologyButton = document.getElementById("inputTypologyTitleButton");
-newTypologyButton.addEventListener('click', addTypology, false);
-/* 
-* Function removeTypology()
-* Removing typology from List.typologies
-* @param {id} number
-*/
-function removeTypology(id){
-    List.deleteTypo(id);
-    loadTypologiesHtml();  
-}
-/* 
-* Function loadTypologiesHtml()
-* Preparing the HTML with typos info and actions and loading the HTML into an specific id
-*/
-function loadTypologiesHtml(){
-    document.getElementById("typologiesList").innerHTML = '';
-    document.getElementById("selectItemTypo").innerHTML = '<option value="">Typology not selected</option>';
-    List.getTypos().forEach((element, index) => {
-        document.getElementById("typologiesList").innerHTML += `<li class="list-group-item">${element} <button type="button" data-id="${index}" data-iName="${element}" data-obj="removeTypo" class="remove btn btn-danger float-sm-end pt-1 pb-1"></button></li>`;
-        document.getElementById("selectItemTypo").innerHTML += `<option value="${element}">${element}</option>`;
-    }); 
-}
-/* 
-* List Items Managment
-* Function addItem()
-* Getting the typology select, item input and quantity input value, item creation
-*/
-function addItem(){ 
-    List.addItem(document.getElementById("inputItemTitle").value, document.getElementById("inputItemQuantity").value, document.getElementById("selectItemTypo").value);
-    loadItemsHtml();
-    document.getElementById("inputItemTitle").value = "";
-    document.getElementById("inputItemQuantity").value = 1;
-    document.getElementById("inputItemTitle").focus();
-}
-const newItemButton = document.getElementById("inputItemTitleButton");
-newItemButton.addEventListener('click', addItem, false);
-/* 
-* Function removeItem()
-* Removing item from List.items
-* @param {id} number
-*/
-function removeItem(id){
-    List.deleteItem(id);
-    loadItemsHtml();
-}
-/* 
-* Function loadItemsHtml()
-* Preparing the HTML with items info and actions and loading the HTML into an specific id
-*/
-function loadItemsHtml(){
-    document.getElementById("itemsList").innerHTML = '';
-    List.getItems().forEach((element, index) => {
-        document.getElementById("itemsList").innerHTML += `<li class="list-group-item">${(element.typology.length > 0) ? element.typology : ""} ${element.title} X ${element.quantity} <button type="button" data-id="${index}" data-iName="${element.title}" data-obj="removeItem" class="remove btn btn-danger float-sm-end pt-1 pb-1"></button></li>`;
-    });
-}
 /********************************************************************************************************/
-/* 
-* Function downloadList()
-+ Generating a {List.title}.txt file whith list title and items formated inside. Auto download.
-*/
-async function downloadList(){
-    let content = `${List.title} \n\nItems : \n\n`;
-    List.getItems().forEach((element, index) => {
-        content += `${index + 1} - ${(element.typology.length > 0) ? element.typology : ""} ${element.title} X ${element.quantity} \n`;
-    })
-    let file = new Blob([content], {
-        type: 'text/txt'
-    });
-    const linkId = document.getElementById('downloadLink');
-    linkId.href = URL.createObjectURL(file);
-    linkId.download = `${List.title}.txt`;
-    linkId.click();
-}
-const downloadListButton = document.getElementById("downloadList");
-downloadListButton.addEventListener('click', downloadList, false);
-/* 
-* Function sendList()
-* Generating formated email body with List title and items. Opening email managment in navigator to send to anyone.
-*/
-function sendList(){ 
-    let body = "Items : %0D%0A%0D%0A";
-    List.getItems().forEach((element, index) => {
-        body += `${index + 1} - ${(element.typology.length > 0) ? element.typology : ""} ${element.title} X ${element.quantity} %0D%0A`;
-    });
-    window.open(`https://mail.google.com/mail/?view=cm&fs=1&su=New List : ${List.title}&body=${body}`);
-}
-const sendListButton = document.getElementById("sendList");
-sendListButton.addEventListener('click', sendList, false);
-/* 
-* Function exportList()
-+ Generating a {List.title}.export.txt file whith all list data (title, typologies and items) formated inside. Download auto.
-* This file could be used to import it as list backup.
-* Working on exporting as JSON file...
-*/
-function exportList(){ 
-    let content = `${List.title}\n`;
-    List.getTyposSortAsc().forEach(element => {
-        content += `${element}|`;
-    });
-    content += '\n';
-    List.getItems().forEach(element => {
-        content += `${(element.typology.length > 0) ? element.typology : ""};${element.title};${element.quantity}|`;
-    })
-    let file = new Blob([content], {type: 'text/txt'});
-    const linkId = document.getElementById('downloadLink');
-    linkId.href = URL.createObjectURL(file);
-    linkId.download = `${List.title}.export.txt`;
-    linkId.click();
-    
-    /* JSON EXPORT 
-    const content = JSON.stringify(List);
-    const file= new Blob([content], {type: "application/json"});
-    const linkId = document.getElementById('downloadLink');
-    linkId.href = URL.createObjectURL(file);
-    linkId.download = `${List.title}.export.json`;
-    linkId.click();
-    */
-    
-}
-const exportListButton = document.getElementById("exportList");
-exportListButton.addEventListener('click', exportList, false);
-/* 
-* Function importList()
-* Uploading previous {List.title}.export.txt file
-*/
-function importList(){ 
-    document.getElementById('importListfile').click();
-}
-const importListButton = document.getElementById("importList");
-importListButton.addEventListener('click', importList, false);
-/* 
-* Function loadList()
-+ Taking an imported {List.title}.export.txt file whith all list data (title, typologies and items) formated inside
-* The information is loaded into the List object and web application
-* Working on importing as JSON file...
-*/
-function loadList(){
-    let fr = new FileReader();
-    fr.onload = function () {
-        fr.result.split('\n').forEach((element, index) => {
-            switch(index){
-                case 0:
-                createListTitle(List.title = element);
-                break;
-                case 1:
-                List.loadImportTypologies(element.split('|'));
-                loadTypologiesHtml();
-                break;
-                case 2:
-                List.loadImportItems(element.split('|'));
-                loadItemsHtml();
-                break;
-            }
-        })
-        /* JSON Import
-        console.log(JSON.parse(fr.result));
-        createListTitle(List.title = fr.result.title);
-        List.typologies = fr.result.typologies;
-        List.items = fr.result.items;
-        loadTypologiesHtml();
-        loadItemsHtml();*/
-    }
-    fr.readAsText(this.files[0]);
-}
-const importListfile = document.getElementById('importListfile')
-importListfile.addEventListener('change', loadList, false);
-/********************************************************************************************************/
-/*
-* Preventing key enter usage and given click action to relationed button
-*/
-window.addEventListener('keydown',function(e) {
-    if (e.keyIdentifier=='U+000A' || e.keyIdentifier=='Enter' || e.keyCode==13) {
-        if (e.target.nodeName=='INPUT' && (e.target.type=='text' || e.target.type=='number')) {
-            e.preventDefault();
-            let target = (e.target.id === 'inputItemQuantity') ? 'inputItemTitle' : e.target.id;
-            document.getElementById(target + 'Button').click();
-        }
-    }
-}, true);
-/*
-* Redirecting to action by click event on delete Typo or delete Item button
-*/
-document.getElementById('typologiesList').addEventListener("click", function(e){
-    //console.log(e.explicitOriginalTarget.attributes.getNamedItem("data-obj").value);
-    if(e.explicitOriginalTarget.attributes.getNamedItem("data-obj").value === 'removeTypo'){
-        if(confirm('Do you want to delete typology ' + e.explicitOriginalTarget.attributes.getNamedItem("data-iName").value + '?')){
-            removeTypology(e.explicitOriginalTarget.attributes.getNamedItem("data-id").value);
-        }
-    }
-}, true);
-document.getElementById('itemsList').addEventListener("click", function(e){
-    if(e.explicitOriginalTarget.attributes.getNamedItem("data-obj").value === 'removeItem'){
-        if(confirm('Do you want to delete item ' + e.explicitOriginalTarget.attributes.getNamedItem("data-iName").value + '?')){
-            removeItem(e.explicitOriginalTarget.attributes.getNamedItem("data-id").value);
-        }
-    }
-}, true);
 /*
 * Function changeVisivilityStateById()
 * Changes an visible or invisible id css class removing oldClass and adding newClass
@@ -245,9 +29,9 @@ document.getElementById('itemsList').addEventListener("click", function(e){
 * param {oldClass} string
 * param {newClass} string
 */
-function changeVisivilityStateById(id, oldClass, newClass){
+function changeVisivilityStateById(id, oldClass, newClass) {
     document.getElementById(id).classList.remove(oldClass);
-    document.getElementById(id).classList.add(newClass);    
+    document.getElementById(id).classList.add(newClass);
 }
 /*
 * Function ableOrDisableListTools()
@@ -255,7 +39,7 @@ function changeVisivilityStateById(id, oldClass, newClass){
 * param {a} bool
 * param {b} bool
 */
-function ableOrDisableListTools(a, b){
+function ableOrDisableListTools(a, b) {
     document.getElementById("inputListTitle").disabled = a;
     document.getElementById("inputListTitleButton").disabled = a;
     document.getElementById("removeListTitle").disabled = b;
